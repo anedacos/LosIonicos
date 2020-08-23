3ba9593a15db7fce3350733c1b60ff3c8ed58ba9
ericksitocg <ericksitocg@hotmail.com>
2020-08-23 17:53:54 -0500

    crear pedido de prueba (quemado) desde checkout

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../services/cart.service';
+import { Router } from '@angular/router';
+import { FirebaseService } from '../services/firebase.service';
+
 
 @Component({
   selector: 'app-checkout',
@@ -10,7 +13,11 @@ export class CheckoutComponent implements OnInit {
   carrito: Product[] = [];
   productos: Product[] =[];
   total=5;
-  constructor() { }
+  constructor(
+    public router: Router,
+    private firebaseService: FirebaseService,
+
+  ) { }
 
   ngOnInit() {
     this.obtenerCarrito();
@@ -19,10 +26,24 @@ export class CheckoutComponent implements OnInit {
 
   obtenerCarrito(){
     this.carrito=JSON.parse(localStorage.getItem("carrito"));
-    console.log(this.carrito);
+
     this.total= this.productos.reduce((i, j) => i + j.price * j.amount, 0);
     return this.carrito
   }
-
-  
+  realizarCompra(){
+    //Debo obtener estos datos desde un form de ionic como en new task
+    let data = {
+      nombre: "Nombre de prueba",
+      direccion: "Direccion de prueba",
+      total:123.21,
+      estado:"Preparando",
+      image:  "./assets/imgs/default_image.jpg"
+    }
+    this.firebaseService.createPedido(data)
+    .then(
+      res => {
+        this.router.navigate(["/pedidos"]);
+      }
+    )
+  }
 }
